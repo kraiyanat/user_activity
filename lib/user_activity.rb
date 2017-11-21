2b26d1267179a04d2efff12a7f864c3f8f12d753
Kraiyanat Tee <tee@iwa.fi>
2017-11-21 09:39:54 +0700

    Modified data added to database table

--- lib/user_activity.rb
+++ lib/user_activity.rb
@@ -10,14 +10,21 @@ module UserActivity
   	@current_user || Struct.new(:name, :id).new("anonymus", "")
   end
 
+  def activity_for_user_activity(controller, method)
+    activity_hash = {}
+    activity_hash[["aa", "bb"]] = "cc"
+
+    activity_hash[[controller.to_s, method.to_s]].to_s
+  end
+
   def log_user_activity
   	UserActivityLog.create(
   													user_id: user_for_user_activity.id,
   													user_name: user_for_user_activity.name,
   													action: params[:action],
   													controller: params[:controller],
-  													activity: "",
-  													http_request: request.body.read
+  													activity: activity_for_user_activity("aa", "bb"),
+  													http_request: "#{request.method} #{request.url} #{request.params}"
   												)
   end
 
@@ -29,7 +36,7 @@ module UserActivity
   end
 end
 
-ActiveSupport.on_load(:action_controller) do   
+ActiveSupport.on_load(:action_controller) do 
   include UserActivity
   before_action :log_user_activity
 end
